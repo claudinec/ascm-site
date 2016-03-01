6210ef7514e082a6db5f36ebb90e35313917218d
Claudine Chionh <cc@claudinec.net>
2016-03-01 17:44:14 +1100

    Revert "Add adaptivetheme and footheme to composer.son. Composer update."
    This reverts commit ce610087c35ce74b2fa9ccf215d83eb6375b465b.

--- web/sites/all/themes/contrib/footheme/template.php
+++ web/sites/all/themes/contrib/footheme/template.php
@@ -1,46 +1,66 @@
 <?php
-// Footheme by Adaptivethemes.com, a starter sub-sub-theme.
 
 /**
+ * @file
+ * Template.php - process theme data for your sub-theme.
+ * 
  * Rename each function and instance of "footheme" to match
  * your subthemes name, e.g. if you name your theme "footheme" then the function
  * name will be "footheme_preprocess_hook". Tip - you can search/replace
  * on "footheme".
  */
 
+
 /**
- * Override or insert variables into the html templates.
- * Replace 'footheme' with your themes name, i.e. mytheme_preprocess_html()
+ * Override or insert variables for the html template.
  */
+/* -- Delete this line if you want to use this function
 function footheme_preprocess_html(&$vars) {
-
-  // Load the media queries styles
-  // If you change the names of these files they must match here - these files are
-  // in the /css/ directory of your subtheme - the names must be identical!
-  $media_queries_css = array(
-    'footheme.responsive.style.css',
-    'footheme.responsive.gpanels.css'
-  );
-  load_subtheme_media_queries($media_queries_css, 'footheme'); // Replace 'footheme' with your themes name
-
- /**
-  * Load IE specific stylesheets
-  * AT automates adding IE stylesheets, simply add to the array using
-  * the conditional comment as the key and the stylesheet name as the value.
-  *
-  * See our online help: http://adaptivethemes.com/documentation/working-with-internet-explorer
-  *
-  * For example to add a stylesheet for IE8 only use:
-  *
-  *  'IE 8' => 'ie-8.css',
-  *
-  * Your IE CSS file must be in the /css/ directory in your subtheme.
-  */
-  /* -- Delete this line to add a conditional stylesheet for IE 7 or less.
-  $ie_files = array(
-    'lte IE 7' => 'ie-lte-7.css',
-  );
-  load_subtheme_ie_styles($ie_files, 'footheme'); // Replace 'footheme' with your themes name
-  // */
-
 }
+function footheme_process_html(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables for the page templates.
+ */
+/* -- Delete this line if you want to use these functions
+function footheme_preprocess_page(&$vars) {
+}
+function footheme_process_page(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the node templates.
+ */
+/* -- Delete this line if you want to use these functions
+function footheme_preprocess_node(&$vars) {
+}
+function footheme_process_node(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the comment templates.
+ */
+/* -- Delete this line if you want to use these functions
+function footheme_preprocess_comment(&$vars) {
+}
+function footheme_process_comment(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the block templates.
+ */
+/* -- Delete this line if you want to use these functions
+function footheme_preprocess_block(&$vars) {
+}
+function footheme_process_block(&$vars) {
+}
+// */
